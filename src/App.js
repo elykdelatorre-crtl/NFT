7af2f70e7bf197d08e8851c342fdbe5356666749
Jade Oloroso <100562473+jdojade@users.noreply.github.com>
2022-04-01 11:48:20 +0800

    Done

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 import Logo from "./components/Logo";
 import {Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ConnectWallet from "./pages/ConnectWallet";
 import NFT from "./pages/NFT";
+import Button from 'react-bootstrap/Button';
+
 
 
 function App() {
--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,37 @@
 import './components.css';
-
 function Logo()
 {
     return(
-        <div className="LogoCon">
-            <img className="Logo" src='./Img/HomequbeLogo.svg' />
-        </div>
+    <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+          </Container>
+        </Navbar>
+    </>      
+            // <nav className="navbar navbar-main navbar-light sticky-top">
+            //     <div className="container">
+            //             <img className="logo" src='./Img/Homeqube-logo-black_small 1.svg' alt=''/>
+                    
+            //             <ul className="navbar-nav">
+            //                 <li className="nav-item ms-5">
+            //                         <p className="nav-item-pipehome px-4 tp-4">
+            //                             stakeholders
+            //                         </p>
+            //                 </li>
+            //                 <li className="nav-item ms-4">
+            //                         <p className="nav-item-QToken px-4 pt-3">
+            //                             qube <br />tokenomics
+            //                         </p>
+            //                 </li>
+            //             </ul>
+            //     </div>
+            // </nav>
     );
 }
 
