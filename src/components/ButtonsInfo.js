1944c90249d59726f79cbf44106a141ab2871047
Jade Oloroso <100562473+jdojade@users.noreply.github.com>
2022-04-03 19:09:12 +0800

    update

--- src/components/ButtonsInfo.js
+++ src/components/ButtonsInfo.js
@@ -38,33 +38,6 @@ function ButtonsInfo()
                                 </div>
                             </div>
                     </div>
-                    <div className='col link d-flex align-items justify-content mt-5 container-fluid'>
-                            <div className='container-fluid'>
-                                <div className='div-position'>
-                                <p className='airdrop-text'>AVAILABLE: 5</p>
-                                <button className="footbar-button btn btn-lg rounded-pill mx-5" type='button' data-bs-toggle="modal"
-                                data-bs-target="#connect-wallet-modal">
-                                   60,000  <img className='iconsol mx-2' src='./Img/iconQ.svg'/>
-                                </button> 
-                                <p className='airdrop-text-2'>1000
-                                    <img className='iconsol mx-2' src='./Img/solLogo.svg'/>
-                                </p>
-                                </div>
-                            </div>
-                            
-                            <div className='container-fluid'>
-                                <div className='div-position'>
-                                <p className='airdrop-text'>AVAILABLE: 5</p>
-                                <button className="footbar-button btn btn-lg rounded-pill mx-5" type='button' data-bs-toggle="modal"
-                                data-bs-target="#connect-wallet-modal">
-                                     60,000  <img className='iconsol mx-2' src='./Img/iconQ.svg'/>
-                                </button> 
-                                <p className='airdrop-text-2'>1000
-                                    <img className='iconsol mx-2' src='./Img/solLogo.svg'/>
-                                </p>
-                                </div>
-                            </div>
-                    </div>
                 </section>
              
             </div>
